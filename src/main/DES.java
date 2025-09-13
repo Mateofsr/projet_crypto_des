26c06a2dd17a5fbb93f986b78e4c114385a4b543
Mateofsr <90698344+Mateofsr@users.noreply.github.com>
2025-09-13 16:35:56 +0200

    feat:stringToBits function

--- src/main/DES.java
+++ src/main/DES.java
@@ -1,5 +1,31 @@
 package main;
 
 public class DES {
-
+	private final int TAILLE_BLOC = 64;
+	private final int TAILLE_SOUS_BLOC = 32;
+	private final int NB_RONDE = 1;
+	private final int[][] PERM_INITIAL = {
+			{57,49,41,33,25,17,9,1},
+			{59,51,43,35,27,19,11,3},
+			{61,53,45,37,29,21,13,5},
+			{63,55,47,39,31,23,15,7},
+			{56,48,40,32,24,16,8,0},
+			{58,50,42,34,26,18,10,2},
+			{60,52,44,36,28,20,12,4},
+			{62,54,46,38,30,22,14,6}};
+	
+	public int[] stringToBits(String message) {
+		byte[] bytes = message.getBytes();
+		int[] bits = new int[bytes.length * 8];
+		for (int i = 0; i < bytes.length; i++) {
+	        for (int j = 0; j < 8; j++) {
+	        	bits[i * 8 + j] = (bytes[i] >> (7 - j)) & 1;
+	        }
+	    }
+		return bits;
+	}
+	
+	private static void main(String[] args) {
+		
+	}
 }
--- src/test/DESTest.java
+++ src/test/DESTest.java
@@ -1,5 +1,34 @@
 package test;
 
-public class DESTest {
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import main.DES;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class DESTest {
+
+	private DES des;
+	
+	@BeforeAll
+	void setUp() {
+		des = new DES();
+	}
+	
+	@Test
+	void testStringToBits() {
+		// GIVEN a string
+		String message = "Hello world!";
+		int[] expectedResult = {0,1,0,0,1,0,0,0,0,1,1,0,0,1,0,1,0,1,1,0,1,1,0,
+				0,0,1,1,0,1,1,0,0,0,1,1,0,1,1,1,1,0,0,1,0,0,0,0,0,0,1,1,1,0,1,
+				1,1,0,1,1,0,1,1,1,1,0,1,1,1,0,0,1,0,0,1,1,0,1,1,0,0,0,1,1,0,0,
+				1,0,0,0,0,1,0,0,0,0,1};
+		// WHEN the string is converted to bits
+		// THEN
+		assertArrayEquals(des.stringToBits(message), expectedResult);
+	}
 }
