db713ef11fcf55fcd04aad8778bfe89810e0927a
Mateofsr <90698344+Mateofsr@users.noreply.github.com>
2025-10-07 16:43:32 +0200

    add clear button, add decrypted text

--- src/main/ui/FenetrePrincipale.java
+++ src/main/ui/FenetrePrincipale.java
@@ -13,7 +13,9 @@ public class FenetrePrincipale extends JFrame {
     private JTextArea texteCrypte;
     private JButton boutonCrypter;
     private JButton boutonDecrypter;
+    private JButton boutonClear;
     private JComboBox<String> modeChoix;
+    private JLabel texteDecrypte;
 
     private Chiffrement chiffrement;
     private int[] encryptedMessage;
@@ -28,19 +30,25 @@ public class FenetrePrincipale extends JFrame {
 
         boutonCrypter = new JButton("Crypter");
         boutonDecrypter = new JButton("Décrypter");
+        boutonClear = new JButton("Nettoyer");
         modeChoix = new JComboBox<>(new String[]{"DES", "TripleDES"});
 
+        texteDecrypte = new JLabel();
+        
         setLayout(new BorderLayout());
-        JPanel panelCentre = new JPanel(new GridLayout(2, 2, 10, 10));
+        JPanel panelCentre = new JPanel(new GridLayout(3, 3, 10, 10));
         panelCentre.add(new JLabel("Texte clair :"));
         panelCentre.add(new JScrollPane(texteClair));
         panelCentre.add(new JLabel("Texte chiffré :"));
         panelCentre.add(new JScrollPane(texteCrypte));
-
+        panelCentre.add(new JLabel("Texte déchiffré :"));
+        panelCentre.add(texteDecrypte);
+        
         JPanel panelBas = new JPanel();
         panelBas.add(modeChoix);
         panelBas.add(boutonCrypter);
         panelBas.add(boutonDecrypter);
+        panelBas.add(boutonClear);
 
         add(panelCentre, BorderLayout.CENTER);
         add(panelBas, BorderLayout.SOUTH);
@@ -64,11 +72,20 @@ public class FenetrePrincipale extends JFrame {
                 if (!texte.isEmpty()) {
 	                String mode = (String) modeChoix.getSelectedItem();
 	                String resultat = chiffrement.decrypter(encryptedMessage,mode);
-	                texteClair.setText(resultat);
+	                texteDecrypte.setText(resultat);
                 }
             }
         });
 
+        
+        boutonClear.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent e) {
+        		texteClair.setText("");
+        		texteCrypte.setText("");
+        		texteDecrypte.setText("");
+        	}
+        });
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setLocationRelativeTo(null);
